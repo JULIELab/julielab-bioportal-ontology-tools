51f10b545d7b039fde593e240e38bed6d6ea6b9a
Bernd <pikatech@mail.de>
2018-02-22 16:28:14 +0100

    Implemented test that demonstrates HttpHandler.sendGetRequest(URI) returning null after aborting instead of throwing an ResourceDownloadException.
    This leads to an IllegalArgumentException when that null is processed.

--- src/test/java/de/julielab/bioportal/ontologies/OntologyDownloaderTest.java
+++ src/test/java/de/julielab/bioportal/ontologies/OntologyDownloaderTest.java
@@ -2,15 +2,26 @@ package de.julielab.bioportal.ontologies;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Field;
+import java.net.SocketTimeoutException;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.http.HttpEntity;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.google.common.collect.Sets;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import de.julielab.bioportal.ontologies.OntologyDownloader;
+import de.julielab.bioportal.ontologies.data.OntologyMetaData;
+import de.julielab.bioportal.util.ResourceAccessDeniedException;
+import de.julielab.bioportal.util.ResourceDownloadException;
+import de.julielab.bioportal.util.ResourceNotFoundException;
 
 /**
  * Only used to download test data. You have to insert your BioPortal API key in
@@ -19,7 +30,7 @@ import de.julielab.bioportal.ontologies.OntologyDownloader;
  * @author faessler
  *
  */
-@Ignore
+
 public class OntologyDownloaderTest {
 
 	@BeforeClass
@@ -29,10 +40,40 @@ public class OntologyDownloaderTest {
 			FileUtils.deleteDirectory(downloadDir);
 	}
 
-	@Test
-	public void testDownload() throws Exception {
-		OntologyDownloader downloader = new OntologyDownloader("YOUR API KEY");
-		downloader.downloadOntologies(new File("src/test/resources/download-test/ontologies"),
-				new File("src/test/resources/download-test/info"), Sets.newHashSet("GRO"));
+//	@Test
+//	public void testDownload() throws Exception {
+//		OntologyDownloader downloader = new OntologyDownloader("YOUR API KEY");
+//		downloader.downloadOntologies(new File("src/test/resources/download-test/ontologies"),
+//				new File("src/test/resources/download-test/info"), Sets.newHashSet("GRO"));
+//	}
+	
+	@Test(expected = ResourceDownloadException.class) 
+	public void testSendGetRequestNullMock() throws IllegalAccessException,
+	IllegalArgumentException, InvocationTargetException, NoSuchMethodException,
+	SecurityException, SocketTimeoutException, ResourceNotFoundException,
+	ResourceAccessDeniedException, ResourceDownloadException, NoSuchFieldException {
+		String dummy = "";
+		File dummyFile = new File("dummy");
+		OntologyMetaData metadata = new OntologyMetaData("Foobar", "FB");
+		
+		//Make private method accessible
+		Class<OntologyDownloader> targetClass = OntologyDownloader.class;
+		Method method = targetClass.getDeclaredMethod("downloadInfoForOntology",
+				dummy.getClass(), dummyFile.getClass(), metadata.getClass(), dummy.getClass());
+		method.setAccessible(true);
+		
+		//Mock method to be called
+		HttpHandler mock = mock(HttpHandler.class);
+		when(mock.sendGetRequest("http://foo")).thenReturn(null);
+
+		//Insert HttpHandler mock into OntologyDownloader via reflection
+		OntologyDownloader downloader = new OntologyDownloader("API key");
+		Class<?> c = downloader.getClass();
+		Field handler = c.getDeclaredField("httpHandler");
+		handler.setAccessible(true);
+		handler.set(downloader, mock);
+		
+		//Call method
+		HttpEntity entity = (HttpEntity) method.invoke(downloader, "http://foo", dummyFile, metadata, "Dummy");
 	}
 }
