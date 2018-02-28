71b9fd8091bfc4ba9fa95819d78aaae241eb81e0
Tech <pikatech@mail.de>
2018-02-28 16:07:12 +0100

    Remove unused imports.

--- src/main/java/de/julielab/bioportal/util/BioPortalToolUtils.java
+++ src/main/java/de/julielab/bioportal/util/BioPortalToolUtils.java
@@ -1,19 +1,13 @@
 package de.julielab.bioportal.util;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.io.Writer;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.concurrent.atomic.AtomicInteger;
